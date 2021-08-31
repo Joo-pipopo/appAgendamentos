0cee130e36b4d5fbe47ef05eccddfef99fc400e8
João Victor <51753615+Joo-pipopo@users.noreply.github.com>
2021-08-31 08:55:26 -0300

    Add files via upload

---
+++ consultar-arquivos/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC0f_mPXUDd7itPh3OVolm850Yrzn3lDRM",
+  authDomain: "app-agendamentos-27699.firebaseapp.com",
+  projectId: "app-agendamentos-27699",
+  storageBucket: "app-agendamentos-27699.appspot.com",
+  messagingSenderId: "74812937973",
+  appId: "1:74812937973:web:870d11b13247f5ba45d7bc",
+  measurementId: "G-0PXVRZDK2H"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
